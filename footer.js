b9ba67c92de9f9f1e0840753477c16339b66ff5b
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2020-09-03 17:29:45 +0900

    added footer

---
+++ footer.js
@@ -0,0 +1,11 @@
+function footer(){
+var html = "";
+html += '<footer>';
+html += '<small>';
+html += '© 2020~  RetoRuto9900K<br>';
+html += '</small>';
+html += '<a style="color: skyblue;" href="https://twitter.com/tutinoko_kusaa">Twitter</a> ';
+html += '&nbsp&nbsp<a style="color: skyblue;" href="https://github.com/tutinoko2048/SeichiServer3rd">Github</a>';
+html += '</footer>';
+document.write(html);
+}
